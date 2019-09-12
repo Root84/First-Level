b5b425163023c540550d8a5f2fa7c883479c365f
Root84 <Noellgen84@aol.com>
2019-09-12 18:04:40 +0200

    test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print ("hello kleiner Kater")
+def get_Temperatur():
+ while True:
+C = input ("Gib die Temperatur in Celsius ein:")   
+try:
+C = float C
+return C
+except ValueError:
+print("Nicht gültig")
+def convert to kelvin():
+K = C + 273,15
+    return K
+print("Das sind" +str(K) + "Kelvin")
